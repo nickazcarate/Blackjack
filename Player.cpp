50f1c6b73081a49777f587cfd1d1ab9918e2b9fa
Asher Johnson Hogan <ajohnsonhogan@ufl.edu>
2018-04-15 15:31:30 -0400

    Added some stuff to Player

--- Player.cpp
+++ Player.cpp
@@ -2,13 +2,83 @@
 
 #include "Player.h"
 
-Player::Player(int money, bool isBot) {
+Player::Player(int money, bool isBot, const Game* game) : game(game){
     this->money = money;
     this->isBot = isBot;
+    gamesWon = 0;
+    gamesLost = 0;
+    gamesTied = 0;
 }
+
 int Player::getMoney(){
     return this->money;
 }
-vector<Card *> Player::getHand(){
+
+vector<Card*> Player::getHand(){
     return this->hand;
+}
+
+void Player::wonGame() {
+    gamesWon++;
+}
+
+void Player::tiedGame() {
+    gamesTied++;
+}
+
+void Player::lostGame() {
+    gamesLost++;
+}
+
+int Player::getWins() {
+    return gamesWon;
+}
+
+int Player::getLosses() {
+    return gamesLost;
+}
+
+int Player::getTies() {
+    return gamesTied;
+}
+
+
+void takeTurn();
+void updateMoney();
+
+void getCard();
+void doubleDown();
+void stand();
+void surrender();
+void split();
+void purchaseInsurance();
+
+void Player::takeTurn() {
+    if (!isBot) {
+        cout << "What would you like to do?";
+    }
+}
+
+void Player::getCard() {
+    // deal card
+}
+
+void Player::doubleDown() {
+
+}
+
+void Player::stand() {
+
+}
+
+void Player::surrender() {
+
+}
+
+bool Player::split() {
+    return true;
+}
+
+void Player::purchaseInsurance() {
+
 }
--- Player.h
+++ Player.h
@@ -3,25 +3,37 @@
 #ifndef BLACKJACK_PLAYER_H
 #define BLACKJACK_PLAYER_H
 #include "Card.h"
+#include "Game.h"
 #include <vector>
 
 
 class Player {
 public:
-    Player(int money, bool isBot);
+    Player(int money, bool isBot, const Game* game);
     int getMoney();
     vector<Card *> getHand();
     void wonGame();
     void lostGame();
     void tiedGame();
+    int getWins();
+    int getLosses();
+    int getTies();
+    void takeTurn();
+    void updateMoney();
 private:
+    const Game* game;
     int money;
     int gamesWon;
     int gamesLost;
     int gamesTied;
     vector<Card *> hand;
     bool isBot;
-
+    void getCard();
+    void doubleDown();
+    void stand();
+    void surrender();
+    bool split();
+    void purchaseInsurance();
 };
 
 
